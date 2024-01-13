d6adf617bfca8df6705a67227cd623ed5dc779cd
davymonte80 <davymonte80@gmail.com>
2024-01-13 11:17:03 +0300

    add phone to the user model

--- app/utils/models/user.js
+++ app/utils/models/user.js
@@ -5,8 +5,13 @@ const UserSchema=new Schema({
         type:String,
         required:true
 
-    }
+    },
     email:{
+        type:String,
+        required:true
+
+    },
+    phone:{
         type:String,
         required:true
 
