ec7fc030c4fe664fe27dfafa1291541d4e0c183e
davymonte80 <davymonte80@gmail.com>
2024-01-13 11:02:04 +0300

    first commit

--- app/utils/database.js
+++ app/utils/database.js
@@ -2,19 +2,23 @@ import mongoose from "mongoose";
 
 let isConnected= false
 
-export const connectToDB=async()=>{
+export const connectToDB=async()=>
+{
     mongoose.set('structQuery', true)
-    if(isConnnected){
+    if(isConnnected)
+    {
         console.log("Mongo is already connected")
         return
     }
+    //wait for database data
     try{
         await mongoose.connect('mongodb://localhost:27017/NextLoginTut')
         isConnected=true
         console.log("connected to mongodb")
         
-        catch(e)
+        Catch(e)
         console.log(e)
+
 
 
     }
